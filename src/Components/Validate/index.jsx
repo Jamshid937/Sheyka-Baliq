042c8b38a496aba343af15a12553f8dc70a3cbd5
Jamshid937 <mirazamovj8@gmail.com>
2023-09-04 22:06:51 +0500

    add form validate

--- src/Components/Validate/index.jsx
+++ src/Components/Validate/index.jsx
@@ -1,10 +1,53 @@
 import React from 'react'
+import { useState } from 'react'
+import swal from 'sweetalert';
 import { useTranslation } from 'react-i18next';
+import Container from '../../layout/container';
+import './validate.css';
 
-import Container from '../../layout/container'
-import './validate.css'
+const initialData ={
+  tell: ''
+}
+
+
 const Validate = () => {
+  const [fields,setFields] = useState(initialData)
+  const [error, setError] = useState({})
   const { t, i18n } = useTranslation();
+
+  const handleChange = (e) => {
+    const {name, value} = e.target
+    setFields({
+       ...fields, [name] : value
+   })
+   }
+var sendtelegram = function(e) {
+e.preventDefault()
+const fieldsEror = {}
+
+
+
+if(!fields.tell.trim()){
+fieldsEror.tell = 'tell is required'
+} else if(!/^(\+998\d{9}|\d{9})$/.test(fields.tell)){
+  fieldsEror.tell = 'telefon raqamingizni to`liq kiriting'
+}
+
+
+setError(fieldsEror)
+
+    if(Object.keys(fieldsEror).length === 0){
+    swal({
+      title: "Good job!",
+      text: "You clicked the button!",
+      icon: "success",
+      button: "Jo`natildi!",
+    });
+
+    setFields(initialData)
+  }
+    
+};
   return (
     <Container>
       <div className="validate" id='input' >
@@ -12,8 +55,9 @@ const Validate = () => {
         <p><p>{t('h9')}</p></p>
 
         <div className='validate__form'>
-            <form>
-                <input type="tel" placeholder='999 99 999 99 99'/>
+            <form onSubmit={sendtelegram}>
+                <input type="tel" name='tell' value={fields.tell} placeholder='telefon raqamingizni' onChange={handleChange}/>
+                 {error.tell && <span className='validate__form__span'>{error.tell}</span>}
                 <button><p>{t('btn')}</p></button>
             </form>
         </div>
